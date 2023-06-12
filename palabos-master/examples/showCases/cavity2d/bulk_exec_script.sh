d34220447ac9bc1fd675eff1bd72b77a3cc6b8a1
Gabriele Rolleri <gabriele@irolleri.it>
2023-06-12 14:40:55 +0200

    Modified script to sleep between runs so that DFE cards are not busy from execution when maxforceidle is executed, added export command for environment variables

--- palabos-master/examples/showCases/cavity2d/bulk_exec_script.sh
+++ palabos-master/examples/showCases/cavity2d/bulk_exec_script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+export LD_LIBRARY_PATH=/opt/maxeler/maxeleros/lib:$LD_LIBRARY_PATH
+
 for ((i=0; i < 9; i++)); do
 	echo "Run $i of 9"
 	for ((j=1; j < 5; j++)); do
 		echo "Number of threads: $j"
+		sleep 10
 		maxforceidle -a
-		mpirun -np $(j) ./cavity2d
+		mpirun -np $j ./cavity2d
 	done
+	sleep 10
 	maxforceidle -a
 	mpirun -np 8 ./cavity2d
 done
