b6311cbbd4ef56782f03109b5d6e36579f7ed23a
Gabriele Rolleri <gabriele@irolleri.it>
2023-11-07 18:58:35 +0100

    Increased time between runs

--- palabos-master/examples/showCases/cavity2d/bulk_exec_script.sh
+++ palabos-master/examples/showCases/cavity2d/bulk_exec_script.sh
@@ -6,11 +6,11 @@ for ((i=0; i < 9; i++)); do
 	echo "Run $i of 9"
 	for ((j=1; j < 5; j++)); do
 		echo "Number of threads: $j"
-		sleep 10
+		sleep 60
 		maxforceidle -a
 		mpirun -np $j ./cavity2d
 	done
-	sleep 10
+	sleep 60
 	maxforceidle -a
 	mpirun -np 8 ./cavity2d
 done
