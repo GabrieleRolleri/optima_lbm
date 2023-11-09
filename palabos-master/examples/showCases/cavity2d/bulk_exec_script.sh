60818fe48e6e3cd125f5651034b479cbec44f0a3
Gabriele Rolleri <gabriele@irolleri.it>
2023-11-09 17:18:37 +0100

    Changed script

--- palabos-master/examples/showCases/cavity2d/bulk_exec_script.sh
+++ palabos-master/examples/showCases/cavity2d/bulk_exec_script.sh
@@ -2,15 +2,16 @@
 
 export LD_LIBRARY_PATH=/opt/maxeler/maxeleros/lib:$LD_LIBRARY_PATH
 
-for ((i=0; i < 9; i++)); do
-	echo "Run $i of 9"
-	for ((j=1; j < 5; j++)); do
-		echo "Number of threads: $j"
-		sleep 60
-		maxforceidle -a
-		mpirun -np $j ./cavity2d
+declare -a array=("cpu" "cpu-buff" "dfe" "dfe-full")
+
+for mode in "${array[@]}"
+do
+	for ((run=0; run < 10; run++)); 
+	do
+		for threads in 1 2 4 8;
+			do
+			echo -e "Mode: $mode \n Run: $run \n Threads: $threads"
+			mpirun -np $threads build/$mode/cavity2d
+		done
 	done
-	sleep 60
-	maxforceidle -a
-	mpirun -np 8 ./cavity2d
 done
